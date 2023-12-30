2e1778f5790a4770b826522616b1754b7fc56621
Brian Ward <brian.ward.92@gmail.com>
2023-12-30 10:58:34 -0500

    cfg: require input path and number of rows. also, feat: compute `TABLE_DIR` for location of input files.

--- kanoodle/base.py
+++ kanoodle/base.py
@@ -1,19 +1,22 @@
+import os
+
 import numpy as np
 
 
+TABLE_DIR = os.path.join(__file__.split("base.py")[0], "tables")
+
+
 class Kanoodle(object):
-
-    def __init__(self, input_path="./input.txt", nrow=3):
-
+    def __init__(self, input_path, nrow):
         # Data/Parse
-        data = open("./input.txt", "r").read()
+        data = open(input_path, "r").read()
         data = np.array([row.split("\t") for row in data.split("\n")])
 
         # Get Blocks
-        is_empty = data==""
+        is_empty = data == ""
         ix = np.where(is_empty.all(axis=1))[0]
-        ix = np.insert(ix,0,-1)
-        self.blocks = [data[(i+1):j] for i,j in zip(ix[:-1], ix[1:])]
+        ix = np.insert(ix, 0, -1)
+        self.blocks = [data[(i + 1) : j] for i, j in zip(ix[:-1], ix[1:])]
 
         # Get Shape
         self.nrow = nrow
